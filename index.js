5654185cd4ed04e4c26432b79736a1d2754bb79a
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-07-01 02:31:57 +0600

    post and patch api done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://endgameuser:q4kjDYNtixUhjwtP@cluster0.7wrk7.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -27,9 +27,21 @@ async function run() {
         const result = await taskCollection.insertOne(data);
         res.send(result);
       });
+    
+    app.patch('/task/:id', async(req, res) =>{
+        const id = req.params.id;
+            const data = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedData = {
+              $set: data
+            }
+            const result = await taskCollection.updateOne(filter, updatedData, options);
+            res.send(result);
+    })
 
     } finally {
-      await client.close();
+    //   await client.close();
     }
   }
   run().catch(console.dir);
