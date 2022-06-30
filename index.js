0e5f6cb2b50376b26f6835422d600cf3f457db4d
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-07-01 05:17:53 +0600

    backend api added

--- index.js
+++ index.js
@@ -12,52 +12,58 @@ const uri = "mongodb+srv://endgameuser:q4kjDYNtixUhjwtP@cluster0.7wrk7.mongodb.n
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
-      await client.connect();
-      const taskCollection = client.db("taskDB").collection("tasks");
-      
-      app.get('/task', async (req, res)=>{
-        const query = {completed: false};
-        const cursor =  taskCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-      });
+        await client.connect();
+        const taskCollection = client.db("taskDB").collection("tasks");
 
-      app.get('/completetask', async (req, res)=>{
-        const query = {completed: true};
-        const cursor =  taskCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-      });
+        app.get('/task', async (req, res) => {
+            const query = { completed: false };
+            const cursor = taskCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
-      app.post('/task', async (req, res)=>{
-        const data = req.body;
-        const result = await taskCollection.insertOne(data);
-        res.send(result);
-      });
-    
-    app.patch('/task/:id', async(req, res) =>{
-        const id = req.params.id;
+        app.get('/completetask', async (req, res) => {
+            const query = { completed: true };
+            const cursor = taskCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.post('/task', async (req, res) => {
+            const data = req.body;
+            const result = await taskCollection.insertOne(data);
+            res.send(result);
+        });
+
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
+        app.patch('/task/:id', async (req, res) => {
+            const id = req.params.id;
             const data = req.body;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updatedData = {
-              $set: data
+                $set: data
             }
             const result = await taskCollection.updateOne(filter, updatedData, options);
             res.send(result);
-    })
+        })
 
     } finally {
-    //   await client.close();
+        //   await client.close();
     }
-  }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
