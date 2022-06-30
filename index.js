dc37b4a53919336b438ce07f17a853bcb1843c9b
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-07-01 01:49:56 +0600

    main task api done

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://endgameuser:q4kjDYNtixUhjwtP@cluster0.7wrk7.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+      await client.connect();
+      const taskCollection = client.db("taskDB").collection("tasks");
+      
+      app.get('/task', async (req, res)=>{
+        const query = {completed: false}
+        const cursor =  taskCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+
+      app.post('/task', async (req, res)=>{
+        const data = req.body;
+        const result = await taskCollection.insertOne(data);
+        res.send(result);
+      });
+
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
